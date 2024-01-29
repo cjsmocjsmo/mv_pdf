9ff64863ee3396e4fa47adba37bf1addb267a621
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-29 00:57:14 -0800

    Refactor file copying logic and add support for BMP and PNG file types

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use md5::compute;
+// use md5::compute;
 pub mod walk_dirs;
 
 fn main() {
-    let vp1 = "/home/teresa/Pictures/AllPics".to_string();
-    let vp2 = "/home/teresa/Pictures/PicCD1".to_string();
-    let vp3 = "/home/teresa/Pictures/PicCD2".to_string();
-    let vp4 = "/home/teresa/Pictures/PicCD3".to_string();
-    let vp5 = "/home/teresa/Pictures/PicISO".to_string();
-    let vp_list = vec![vp1, vp2, vp3, vp4, vp5];
-    for vid_path in vp_list {
-        let vid_out_path = "/home/teresa/Documents/PDF/".to_string();
-        let vid_list = walk_dirs::walk_dir(vid_path);
-        for vid in vid_list {
-            let ext_split = vid.split(".").collect::<Vec<&str>>();
-            let ext = ext_split.last().unwrap();
-            let digest = compute(&vid);
-            let new_out_path = format!("{}{:?}.{}", vid_out_path, digest, ext);
-            println!("{} -> {}", vid, new_out_path);
-            std::fs::copy(vid, new_out_path).unwrap();
-        }
+    // let bmp_path = "media/pipi/0123-4567/BMP/";
+    // let png_path = "media/pipi/0123-4567/PNG/";
+    let apath = "/media/pipi/0123-4567/JPG_Master".to_string();
 
-    }
+
+
+
+
+
+    
+    //     let vid_out_path = "/home/teresa/Documents/PDF/".to_string();
+        let _vid_list = walk_dirs::walk_dir(apath);
+    //     for vid in vid_list {
+    //         let ext_split = vid.split(".").collect::<Vec<&str>>();
+    //         let ext = ext_split.last().unwrap();
+    //         let digest = compute(&vid);
+    //         let new_out_path = format!("{}{:?}.{}", vid_out_path, digest, ext);
+    //         println!("{} -> {}", vid, new_out_path);
+    //         std::fs::copy(vid, new_out_path).unwrap();
+    //     }
+
+    // }
 }
--- src/walk_dirs.rs
+++ src/walk_dirs.rs
@@ -1,10 +1,15 @@
-// use std::fs;
+use std::io::Read;
+use std::io::Write;
 use walkdir::WalkDir;
+
 
 pub fn walk_dir(apath: String) -> Vec<String> {
     let mut keeper_vec = Vec::new();
-    let mut idx = 0;
+    // let mut idx = 0;
     let ext_list = ["pdf", "PDF"];
+    let bmp_path = "media/pipi/0123-4567/BMP/".to_string();
+    let png_path = "media/pipi/0123-4567/PNG/".to_string();
+
 
     for e in WalkDir::new(apath)
         .follow_links(true)
@@ -12,16 +17,43 @@ pub fn walk_dir(apath: String) -> Vec<String> {
         .filter_map(|e| e.ok())
     {
         if e.metadata().unwrap().is_file() {
-            idx += 1;
+
+            
             let fname = e.path().to_string_lossy().to_string();
             let ext_split = fname.split(".").collect::<Vec<&str>>();
             let ext = ext_split.last().unwrap();
+
+            let fn_split = fname.split("/").collect::<Vec<&str>>();
+            let file_name = fn_split.last().unwrap();
+
+            if ext == &"bmp" {
+                let new_path = bmp_path.clone() + file_name;
+                // open and read fname as bytes
+                let mut f = std::fs::File::open(fname.clone()).unwrap();
+                let mut buffer = Vec::new();
+                f.read_to_end(&mut buffer).unwrap();
+                // write buffer to new_path
+                let mut f = std::fs::File::create(new_path).unwrap();
+                f.write_all(&buffer).unwrap();
+            } else if ext == &"png" {
+                let new_path = png_path.clone() + file_name;
+                // open and read fname as bytes
+                let mut f = std::fs::File::open(fname.clone()).unwrap();
+                let mut buffer = Vec::new();
+                f.read_to_end(&mut buffer).unwrap();
+                // write buffer to new_path
+                let mut f = std::fs::File::create(new_path).unwrap();
+                f.write_all(&buffer).unwrap();
+            } else {
+                print!("FUCK")
+            }
+
             if ext_list.contains(ext) {
                 keeper_vec.push(fname.clone());
             };
         };
     }
-    println!("Total files: {}\n", idx);
+    // println!("Total files: {}\n", idx);
 
     keeper_vec
 }
